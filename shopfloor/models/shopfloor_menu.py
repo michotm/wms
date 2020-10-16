6e7e80a104908878064ab5cb44dc74517b256291
hparfr <raph@hpar.fr>
2020-10-16 16:55:14 +0200

    Start work on reception

--- shopfloor/models/shopfloor_menu.py
+++ shopfloor/models/shopfloor_menu.py
@@ -74,6 +74,7 @@ class ShopfloorMenu(models.Model):
             ("checkout", "Checkout/Packing"),
             ("delivery", "Delivery"),
             ("location_content_transfer", "Location Content Transfer"),
+            ("reception", "Reception"),
         ]
 
     @api.depends("scenario", "picking_type_ids")
--- shopfloor/services/__init__.py
+++ shopfloor/services/__init__.py
@@ -18,6 +18,7 @@ from . import cluster_picking
 from . import delivery
 from . import location_content_transfer
 from . import single_pack_transfer
+from . import reception
 
 # forms
 from . import forms
