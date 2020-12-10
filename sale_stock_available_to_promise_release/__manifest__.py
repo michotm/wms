3e3d9c198a801f685f6c4f7719ac71f0f3b3ed2e
Sébastien BEAU <sebastien.beau@akretion.com>
2020-12-10 11:17:55 +0100

    [IMP] set module uninstallable

--- sale_stock_available_to_promise_release/__manifest__.py
+++ sale_stock_available_to_promise_release/__manifest__.py
@@ -1,12 +1,11 @@
 # Copyright 2019 Camptocamp (https://www.camptocamp.com)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Available to Promise Release - Sale Integration",
-    "version": "13.0.1.5.0",
+    "version": "13.0.1.3.0",
     "summary": "Integration between Sales and Available to Promise Release",
     "author": "Camptocamp,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/wms",
     "category": "Stock Management",
     "depends": ["sale_stock", "stock_available_to_promise_release", "delivery"],
     "data": [
@@ -15,8 +14,8 @@
         "views/product_product_views.xml",
         "views/sale_order_views.xml",
     ],
-    "installable": True,
-    "license": "LGPL-3",
+    "installable": False,
+    "license": "AGPL-3",
     "application": False,
     "development_status": "Alpha",
 }
--- stock_available_to_promise_release/__manifest__.py
+++ stock_available_to_promise_release/__manifest__.py
@@ -1,12 +1,11 @@
 # Copyright 2019 Camptocamp (https://www.camptocamp.com)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Available to Promise Release",
-    "version": "13.0.1.11.0",
+    "version": "13.0.1.9.2",
     "summary": "Release Operations based on available to promise",
     "author": "Camptocamp,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/wms",
     "category": "Stock Management",
     "depends": ["stock"],
     "data": [
@@ -18,8 +17,8 @@
         "views/res_config_settings.xml",
         "wizards/stock_release_views.xml",
     ],
-    "installable": True,
-    "license": "LGPL-3",
+    "installable": False,
+    "license": "AGPL-3",
     "application": False,
     "development_status": "Alpha",
 }
