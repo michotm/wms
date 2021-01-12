2a4669349fe5bdd76bed3e093e14d3666918b8f9
hparfr <raph@hpar.fr>
2021-01-12 23:56:14 +0100

    [MIG] stock_picking_consolidation_priority fix prio value
    Adapt to new priority values (introduced in v14
    https://github.com/odoo/odoo/commit/e40553723b893bcd )
    Add consolidate_priority setting in the view

--- stock_picking_consolidation_priority/__manifest__.py
+++ stock_picking_consolidation_priority/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Stock Management",
     "depends": ["stock"],
-    "data": [],
+    "data": ["views/stock_picking_type.xml"],
     "installable": True,
     "license": "AGPL-3",
 }
