ee93e9f78b7a6809d7220fb9a14a9e05757dd6e2
hparfr <raph@hpar.fr>
2021-01-12 23:08:41 +0100

    [MIG] stock_picking_consolidation_priority: Migration to 14.0

--- stock_picking_consolidation_priority/__manifest__.py
+++ stock_picking_consolidation_priority/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Transfers Consolidation Priority",
     "summary": "Raise priority of all transfers for a chain when started",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Stock Management",
