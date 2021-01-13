50fbf059ec9c0f26c1afe019c2dea13245b058e8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-01-13 14:39:29 +0100

    Change shopfloor_menu.profile_ids to Many2one
    * It allows grouping and providing stats per menu/profile (example:
      number of move lines to process for a profile).
    * A migration script changes the relation and duplicates a menu if it
      had several profiles

--- shopfloor/__manifest__.py
+++ shopfloor/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Shopfloor",
     "summary": "manage warehouse operations with barcode scanners",
-    "version": "13.0.3.1.0",
+    "version": "13.0.2.1.0",
     "development_status": "Alpha",
     "category": "Inventory",
     "website": "https://github.com/OCA/wms",
@@ -19,12 +19,11 @@
         "stock_picking_batch",
         "base_jsonify",
         "base_rest",
-        "rest_log",
         "base_sparse_field",
         "auth_api_key",
         #  OCA / stock-logistics-warehouse
         "stock_picking_completion_info",
-        #  OCA / stock-logistics-workflow
+        #  OCA / stock-logistics-warehouse
         "stock_quant_package_dimension",
         #  OCA / stock-logistics-warehouse
         "stock_quant_package_product_packaging",
@@ -37,20 +36,24 @@
         # TODO: used for package.package_storage_type_id detail info.
         # This must be an optional dep
         "stock_storage_type",
-        # TODO: used for picking.carrier_id detail info
-        # and to validate packaging/carrier in checkout scenario
+        # TODO: used for picking.carrier_id detail info.
         # This must be an optional dep
         "delivery",
         #  OCA / product-attribute
         "product_packaging_type",
     ],
     "data": [
+        "data/ir_config_parameter_data.xml",
+        "data/ir_cron_data.xml",
         "security/ir.model.access.csv",
+        "views/res_partner.xml",
         "views/shopfloor_menu.xml",
         "views/stock_picking_type.xml",
         "views/stock_location.xml",
         "views/stock_move_line.xml",
+        "views/stock_picking_views.xml",
         "views/shopfloor_profile_views.xml",
+        "views/shopfloor_log_views.xml",
         "views/menus.xml",
     ],
     "demo": [
