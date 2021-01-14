fa9fdfd0a992ebec70f63a30192b35fa2d668585
hparfr <raph@hpar.fr>
2021-01-14 10:50:30 +0100

    !fixup stock_picking_consolidation_priority add setup

--- stock_picking_consolidation_priority/__manifest__.py
+++ stock_picking_consolidation_priority/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Raise priority of all transfers for a chain when started",
     "version": "14.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/wms",
     "category": "Stock Management",
     "depends": ["stock"],
     "data": ["views/stock_picking_type.xml"],
