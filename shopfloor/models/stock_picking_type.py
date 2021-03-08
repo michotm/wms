dccb2833ea6899057d3661ffb9850b818c60ada3
hparfr <raph@hpar.fr>
2021-03-08 12:06:48 +0100

    Fix merge issue related to pr 161

--- shopfloor/models/stock_picking_type.py
+++ shopfloor/models/stock_picking_type.py
@@ -21,11 +21,6 @@ class StockPickingType(models.Model):
         string="Activate Scan and Pack",
         help="For packing force scanning of all product that will be in the delivery",
     )
-    shopfloor_display_packing_info = fields.Boolean(
-        string="Display customer packing info",
-        help="For the Shopfloor Checkout/Packing scenarios to display the"
-        " customer packing info.",
-    )
 
     @api.constrains("show_entire_packs")
     def _check_move_entire_packages(self):
