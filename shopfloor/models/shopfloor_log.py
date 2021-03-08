07e5cbae6228dd7d1810a0e1694a6dd4b8fe16ba
hparfr <raph@hpar.fr>
2021-03-08 11:46:17 +0100

    Fix missing import

--- shopfloor/models/shopfloor_log.py
+++ shopfloor/models/shopfloor_log.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import models, fields
 
 # TODO: drop this model on next release.
 # The feature has been moved to `rest_log`.
