3ab8c99ab6c4e8b0167e7c62d962a2f91ba8310c
Francois Poizat <francois.poizat@gmail.com>
2021-09-13 16:49:08 +0200

    shopfloor_mobile: inventory: Implements start screen and select location in scan_product screen

---
+++ shopfloor_mobile/static/wms/src/components/inventory/line_by_dest.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2020 Akretion SA (http://www.akretion.com)
+ * @author Francois Poizat <francois.poizat@gmail.com>
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+ */
+
+Vue.component("inventory-by-dest", {
+    props: [
+        "inventoryLines",
+        "fields",
+        "lastScanned",
+        "currentLocation",
+    ],
+    methods: {
+        isLastScanned(product) {
+            return (
+                product &&
+                ((product.barcode !== null && product.barcode === this.lastScanned) ||
+                    (product.barcodes.length > 0 && product.barcodes.includes(this.lastScanned)))
+            );
+        },
+    },
+    computed: {
+        linesByLocation: function() {
+            const lines = this.inventoryLines
+                .map(line => {
+                    return {
+                        name: line.product.display_name,
+                        id: line.id,
+                        location: line.location,
+                        barcodes: line.product.barcodes,
+                        qty: line.product_qty,
+                    };
+                })
+                .sort((a, b) => (a.done ? 1 : -1));
+
+            const selected = lines.find(this.isLastScanned) || {};
+            selected.selected = true;
+
+            const locations = lines
+                .map(line => line.location)
+                .filter((value, i, array) => {
+                    return array.findIndex(v => v.id === value.id) === i;
+                });
+
+            const locationWithLines = locations
+                .map(location => {
+                    return {
+                        location,
+                        lines: lines
+                            .filter(line => line.location.id === location.id)
+                            .sort((a, b) => (!a.selected ? 1 : -1)),
+                    };
+                })
+                .filter(location => location.lines.length > 0)
+                .sort((a, b) => (a.name < b.name ? 1 : -1));
+
+            const pivotIndex = locationWithLines.findIndex(
+                location => location.location.id === this.currentLocation
+            );
+
+            if (pivotIndex !== -1) {
+                const removed = locationWithLines.splice(0, pivotIndex);
+                locationWithLines.push(...removed);
+            }
+
+            return locationWithLines;
+        },
+    },
+    template: `
+        <v-container class="mb-16">
+            <div v-for="location in linesByLocation" :key="location.id">
+                <item-detail-card
+                    :record="location.location"
+                    :card_color="utils.colors.color_for(currentLocation === location.location.id ? 'detail_main_card_selected' : 'detail_main_card')"
+                    />
+                <detail-simple-product
+                    v-if="currentLocation === product.location.id"
+                    v-for="product in location.lines"
+                    :product="product"
+                    :fields="fields"
+                    :key="product.id"
+                    :selected="product.selected"
+                    >
+                </detail-simple-product>
+            </div>
+        </v-container>
+    `,
+});
--- shopfloor_mobile/static/wms/src/scenario/inventory.js
+++ shopfloor_mobile/static/wms/src/scenario/inventory.js
@@ -14,6 +14,19 @@ const Inventory = {
                     :inventories="state.data.inventories"
                     :fields="state.fields"
                 />
+                <searchbar
+                    v-if="state_is('scan_product')"
+                    :input_placeholder="scan_placeholder"
+                    :refocusInput="true"
+                    v-on:found="on_scan"
+                />
+                <inventory-by-dest
+                    v-if="state_is('scan_product')"
+                    :inventoryLines="state.data.inventory_lines"
+                    :fields="state.fields"
+                    :currentLocation="state.data.selected_location"
+                    :lastScanned="lastScanned"
+                />
         </Screen>
     `,
     methods: {
@@ -22,19 +35,18 @@ const Inventory = {
         },
     },
     data: function() {
-        // TODO: add a title to each screen
         return {
             usage: "inventory",
             initial_state_key: "start",
             currentLocation: null,
-            lastPickedLine: null,
+            lastScanned: null,
             states: {
                 start: {
                     on_scan: () => {},
                     onSelectInventory: inventory_id => {
-                        this.wait_call(this.odoo.call("select_inventory"), {
+                        this.wait_call(this.odoo.call("select_inventory", {
                             inventory_id,
-                        });
+                        }));
                     },
                     enter: () => {
                         if (!this.state_get_data("start").inventories) {
@@ -42,6 +54,39 @@ const Inventory = {
                         }
                     },
                     fields: [
+                        {
+                            label: "Locations",
+                            path: "locations",
+                            renderer: (rec, field) => _.result(rec, field.path).map(loc => loc.name).join(', '),
+                        },
+                        {
+                            label: "Products",
+                            path: "products",
+                            renderer: (rec, field) => _.result(rec, field.path).length > 0 ? _.result(rec, field.path).map(loc => loc.name).join(', ') : "All",
+                        },
+                        {
+                            label: "Date",
+                            path: "date",
+                            renderer: (rec, field) => this.utils.display.render_field_date(rec, field),
+                        },
+                    ],
+                },
+                scan_product: {
+                    on_scan: ({text}) => {
+                        this.wait_call(this.odoo.call("select_location", {
+                            inventory_id: this.state.data.inventory_id,
+                            location_barcode: text,
+                        }));
+                    },
+                    fields: [
+                        {
+                            label: "Location",
+                            path: "location.name",
+                        },
+                        {
+                            label: "Counted",
+                            path: "qty",
+                        },
                     ],
                 },
             },
