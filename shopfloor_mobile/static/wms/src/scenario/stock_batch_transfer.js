8b6bede16f78c84cf5f9cdf16fffdb0dfe786694
Francois Poizat <francois.poizat@gmail.com>
2021-12-02 20:48:29 +0100

    shopfloor_mobile: stock batch transfer: bigger emphasis on suggested destination

--- shopfloor_mobile/static/wms/src/scenario/stock_batch_transfer.js
+++ shopfloor_mobile/static/wms/src/scenario/stock_batch_transfer.js
@@ -225,11 +225,11 @@ const StockBatchTransfer = {
                         }
                     },
                     fields: [
-                        {path: "supplierCode", label: "Vendor code", klass: "loud"},
+                        {path: "dest.name", label: "Should be put in", klass: "extra-loud"},
+                        {path: "picking_dest.name", label: "Picking destination"},
                         {path: "qty", label: "Quantity"},
                         {path: "qtyDone", label: "Done"},
-                        {path: "picking_dest.name", label: "Picking destination"},
-                        {path: "dest.name", label: "Should be put in"},
+                        {path: "supplierCode", label: "Vendor code"},
                     ],
                 },
             },
